dfe20c8eb5a8096d98f74a841e5e5c1d9ebee60a
Saira493 <sabirsara493@gmail.com>
2026-08-26 12:13:02 +0100

    Update timesheet.py

--- timesheet.py
+++ timesheet.py
@@ -257,7 +257,7 @@ elif st.session_state.auth_status and st.session_state.current_role == "EMPLOYEE
             "EVA International", "Fidelis College", "IQRA ELM", "Heretoga",
             "Tarbiya", "Clarity Housing", "Collfin", "Leicester Islamic Academy",
             "Marathon School", "Suffah Primary School", "HornsBy House", "Vestro Marketing",
-            "Health Care", "SPC Coatings", "UIKAM"
+            "Health Care", "SPC Coatings", "Avicenna", "UIKAM"
         ]
 
         tab1, tab2 = st.tabs(["🕐 Clock In / Clock Out", "📋 Manual Timesheet Entry"])
